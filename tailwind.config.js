931ada18fc93e03e7d7cb4c125304f003f3c75ab
jagadeeshboi <jagadeeshboina@gmail.com>
2024-12-30 20:32:05 +0530

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,19 +5,22 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-    },
-    animation: {
-        'infinite-scroll': 'infinite-scroll 5s linear infinite',
-    },
-    keyframes: {
-        'infinite-scroll': {
-            from: { transform: 'translateX(10)' },
-            to: { transform: 'translateX(-100%)' },
-        }
+    extend: { keyframes: {
+      marquee: {
+        '0%': { transform: 'translateX(0%)' },
+        '100%': { transform: 'translateX(-100%)' },
       },
+      marquee2: {
+        '0%': { transform: 'translateX(100%)' },
+        '100%': { transform: 'translateX(0%)' },
+      },
+    },
+    animation : {
+      'spin-slow-30': 'spin 30s linear infinite',
+      'spin-slow-25': 'spin 25s linear infinite',
+      'spin-slow-10': 'spin 10s linear infinite',
+      'marquee-infinite' : 'marquee 25s linear infinite',
+    },
     },
   },
   plugins: [],
