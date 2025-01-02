0a7096a0bebf9d4bb8a10ed0675eb143f61c9590
jagadeeshboi <jagadeeshboina@gmail.com>
2025-01-02 16:02:46 +0530

    commited 02-01

--- src/Components/Skills.tsx
+++ src/Components/Skills.tsx
@@ -1,40 +1,39 @@
-import { useEffect, useRef } from "react";
 
 function Skills() {
-    const logosRef = useRef<HTMLUListElement | null>(null);
-
-    useEffect(() => {
-        const ul = logosRef.current;
-        if (ul) {
-            ul.insertAdjacentHTML('afterend', ul.outerHTML);
-            const nextSibling = ul.nextSibling as Element | null;
-            if (nextSibling) {
-                nextSibling.setAttribute('aria-hidden', 'true');
-            }
-        }
-    }, []);
     return (
-        <div className="w-full  mx-auto px-4 ">
-            <div className="text-center">
-                <h2 className="text-2xl font-semibold text-gray-800 dark:text-white mb-6">My Skills</h2>
-                <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
-                    <ul ref={logosRef} className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-marquee-infinite">
-                        <li>
-                            <img src="React.png" alt="React" className="w-16 grayscale" />
-                        </li>
-                        <li>
-                            <img src="Python.png" alt="Python" className="w-16 grayscale" />
-                        </li>
-                        <li>
-                            <img src="sql.png" alt="Sql" className="w-16 grayscale" />
-                        </li>
-                        <li>
-                            <img src="typescript.png" alt="typescript" className="w-16 grayscale" />
-                        </li>
-                    </ul>                
-                </div>                
-            </div>
+        <section className=" text-white pt-8 pb-4">
+        <h2 className="text-center text-2xl mb-2 font-bold leading-8 ">SKILLS</h2>
+        {/* <h2 className="text-center text-lg font-extralight leading-8 ">That I have gain Knowledge</h2> */}
+        
+        <div className="logos group relative overflow-hidden whitespace-nowrap  py-10 [mask-image:_linear-gradient(to_right,_transparent_0,_white_128px,white_calc(100%-128px),_transparent_100%)]">
+           <div className="animate-slide-left-infinite group-hover:animation-pause inline-block w-max">
+            <img className="mx-8 inline h-16 grayscale" src="Python.png" alt="Transistor" />
+            <img className="mx-8 inline h-16 grayscale" src="React.png" alt="Reform" />
+            <img className="mx-8 inline h-16 grayscale" src="sql.png" alt="Tuple" />
+            <img className="mx-8 inline h-16 grayscale" src="typescript.png" alt="SavvyCal" />
+            <img className="mx-8 inline h-16 grayscale" src="Python.png" alt="SavvyCal" />
+            <img className="mx-8 inline h-16 grayscale" src="React.png" alt="SavvyCal" />
+          </div>
+      
+          <div className="animate-slide-left-infinite group-hover:animation-pause inline-block w-max">
+            <img className="mx-8 inline h-16 grayscale" src="Python.png" alt="Transistor" />
+            <img className="mx-8 inline h-16 grayscale" src="React.png" alt="Reform" />
+            <img className="mx-8 inline h-16 grayscale" src="sql.png" alt="Tuple" />
+            <img className="mx-8 inline h-16 grayscale" src="typescript.png" alt="SavvyCal" />
+            <img className="mx-8 inline h-16 grayscale" src="Python.png" alt="SavvyCal" />
+            <img className="mx-8 inline h-16 grayscale" src="sql.png" alt="SavvyCal" />
+          </div>
+
+          <div className="animate-slide-left-infinite group-hover:animation-pause inline-block w-max">
+            <img className="mx-8 inline h-16 grayscale" src="Python.png" alt="Transistor" />
+            <img className="mx-8 inline h-16 grayscale" src="React.png" alt="Reform" />
+            <img className="mx-8 inline h-16 grayscale" src="sql.png" alt="Tuple" />
+            <img className="mx-8 inline h-16 grayscale" src="typescript.png" alt="SavvyCal" />
+            <img className="mx-8 inline h-16 grayscale" src="Python.png" alt="SavvyCal" />
+            <img className="mx-8 inline h-16 grayscale" src="sql.png" alt="SavvyCal" />
+          </div>
         </div>
+      </section>
     )
 }
 
